e4a4014843f299ff9617aa436f82898824c1591e
110gituser <2516483574@qq.com>
2025-05-10 21:15:36 +0800

    0.0.0

---
+++ src/pnc_map/pnc_map.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "pnc_map.h"
+using std::cout, std::endl;
+
+void PNC_Map::mapInfo(){
+    cout<<"this is pnc_map\n";
+    cout<<"this is pnc_map\n";
+    cout<<"this is pnc_map\n";
+    cout<<"this is pnc_map\n";
+    cout<<"this is pnc_map\n";
+}
